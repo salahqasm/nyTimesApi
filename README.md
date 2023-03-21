83b168fd1ea991143cc4693f6aecfb9be79cdfe8
salah <salahqasm@outlook.com>
2023-03-21 19:01:03 +0300

    done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nyTimesApi
-advDB assignment
+advDB assignment1:
+call a nyTimes news developer API to get the data
+these data will become in JSON format.
+we should manipulate it and push it to SQL database.
---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const { Sequelize, DataTypes } = require("sequelize");
+const axios = require("axios");
+
+const psql = "postgres://postgres:postgres@localhost:5432/salah"
+const sequelize = new Sequelize(psql, {});
+
+const start = (port) => {
+    app.listen(port, () => {
+        console.log(`********************Server running on port: ${port}****************************`);
+    })
+};
+
+const test = sequelize.define('test', {
+    title: {
+        type: DataTypes.STRING
+    },
+    author: {
+        type: DataTypes.STRING
+    },
+    summary: {
+        type: DataTypes.TEXT
+    }
+}, { timestamps: false });
+
+sequelize.sync({ force: true }).then(() => {
+    console.log(`********************Database Connected Successfuly****************************`);
+    start(3001);
+}).then(async () => {
+    let result;
+    await axios.get('https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=dfCTQmcAYXGiqaB0HxUr4ItRzc3Kyb6P').then(
+        (res)=>{
+            result=res.data;
+        }
+    )
+    result.results.map(elem=>{
+        test.create({
+            title:elem.book_title,
+            author:elem.book_author,
+            summary:elem.summary
+        })
+    })
+});
+
+
+
+
+
+
+
+
